962a481ca8b18a7043f6437bbf9cb4b67d6f4a8e
alankbabu <alankbabu2001@gmail.com>
2025-10-09 11:55:37 +0530

    new commit

---
+++ EmployeeDepartment.entity/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDepartment.entity.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName {  get; set; }
+        public string Email {  get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
